85e0fa76008009248f54bef0d8853bee95950d56
Ananya gupta <150508958+Ananya2627@users.noreply.github.com>
2024-03-10 05:57:38 +0000

    change

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+let a="Hello"
+console.log(typeof a)
+console.log(typeof null); //gives object
+console.log(typeof undefined) //gives undefined
+/*undefined is a datatype while null is an object*/
+
