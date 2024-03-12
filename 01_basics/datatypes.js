fe0f24a6beb70132ee5c30abf56a26310063e437
Ag2627 <162863455+Ag2627@users.noreply.github.com>
2024-03-12 06:40:36 +0000

    datatype summarey

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+/* primitive--ye call by value hote hai
+7 types-> string, number,boolean,null,undefined,symbol,Bigint
+Reference type(non primitive)...memory reference given
+
+array,objects,functions
+JavaScript is a dynamically typed language
+
+*/
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id===anotherid); //false
+console.log(id);
+console.log(anotherid);
+const bignum=5667168n; //n for bigint, typeof use karke undefined aata hai
+const arr=["heello","ananya"]
+const myobj={
+    name:"Ananya",
+    age:"18",
+}
+const myfunction=function(){
+    console.log("Hello world");
+}
+console.log(arr);
+console.log(myobj);
